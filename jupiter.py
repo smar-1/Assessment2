b0975f79623c178852a2e662909ee55402ec22ff
Amari Sisulu <amarisisulu@gmail.com>
2024-01-17 15:32:42 +0000

    imported and installed panas and numpy, created basic moons class which has atribute data containing the entire moons database. Also added moons database to the folder

---
+++ jupiter.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+class Moons:
+    def __init__(self):
+
+        database_service = "sqlite"
+        database = "jupiter.db"
+        connectable = f"{database_service}:///{database}"
+        query = "SELECT * FROM Moons"
+        data = pd.read_sql(query, connectable)
+
+        self.data = data
+
+        pass
+
