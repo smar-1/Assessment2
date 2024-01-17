bdea634e819c8334c66a9f31dd85d8573b5063cf
Amari Sisulu <amarisisulu@gmail.com>
2024-01-17 18:25:55 +0000

    added basic plot method and imported seaborn package

--- jupiter.py
+++ jupiter.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+
+
 class Moons:
     def __init__(self):
-
         database_service = "sqlite"
         database = "jupiter.db"
         connectable = f"{database_service}:///{database}"
@@ -14,5 +16,6 @@ class Moons:
     def extract(self, moon_name):
         return self.data.loc[self.data["moon"] == moon_name]
 
-
-
+    def plot(self, x_var, y_var):
+        sns.relplot(self.data, x=x_var, y=y_var)
+        pass
