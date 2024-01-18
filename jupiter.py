3d302f439639165c1968904b97836d95b7e4e566
Amari Sisulu <amarisisulu@gmail.com>
2024-01-18 18:39:08 +0000

    added a training method which adds 2 columns to the data table T_sq and a_cu. These are The values of T**2 and a**3 for our model.Next it uses sklean and splits the data into training and testing data which are then made to be atribues of the Moons class. Then it creates a linear regression model and also sets this to be an atribute for the Moons class and returns it. Also imported sklean

--- jupiter.py
+++ jupiter.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 class Moons:
@@ -13,14 +16,17 @@ class Moons:
 
         self.data = data
 
-    def extract(self, moon_name):
+    def extract_row(self, moon_name):
         return self.data.loc[self.data["moon"] == moon_name]
+
+    def extract_col(self, col_name):
+        return self.data[col_name]
 
     def correlation(self, x_var, y_var):
         return self.data[x_var].corr(self.data[y_var])
 
     def plot(self, x_var, y_var):
-        sns.relplot(self.data, x=x_var, y=y_var)
+        sns.relplot(data=self.data, x=x_var, y=y_var)
         pass
 
     def max_col(self, col):
@@ -37,3 +43,16 @@ class Moons:
 
     def full_summary(self):
         return self.data.describe()
+
+    def training(self):
+        self.data['T_sq'] = (self.data['period_days']) ** 2
+        self.data['a_cu'] = (self.data['distance_km']) ** 3
+
+        X = self.data[['T_sq']]
+        Y = self.data['a_cu']
+
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(X, Y, test_size=0.3)
+
+        self.model_y = linear_model.LinearRegression(fit_intercept=True)
+        self.model_y.fit(self.x_train, self.y_train)
+        return self.model_y
