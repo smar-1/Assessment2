8bfca7e82d10d47d2b4d5a4d75243119720a4946
Amari Sisulu <amarisisulu@gmail.com>
2024-01-23 21:48:10 +0000

    added method called evaluation which plots rediduals and also plots actual vs predicted values of a_cu. Also changed testing method so it now also returns mse and now r2, mse and test_predict_y are all atributes to the class

--- jupiter.py
+++ jupiter.py
@@ -4,16 +4,21 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
 
 
 class Moons:
     def __init__(self):
+        self.test_predict_y = None
         self.estimated_mass_jupiter = None
         self.model_y = None
         self.y_test = None
         self.y_train = None
         self.x_test = None
         self.x_train = None
+        self.r2 = None
+        self.mse = None
+        self.predict_y = None
         database_service = "sqlite"
         database = "jupiter.db"
         connectable = f"{database_service}:///{database}"
@@ -38,9 +43,6 @@ class Moons:
         sns.relplot(data=self.data, x=x_var, y=y_var)
         pass
 
-    def peek(self, rows):
-        return self.data.head(rows)
-
     def max_col(self, col):
         return self.data[col].max()
 
@@ -64,6 +66,9 @@ class Moons:
         print(self.data)
         pass
 
+    def peek(self, rows):
+        return self.data.head(rows)
+
     def training(self):
         self.data['T_sq'] = (self.data['period_days'] * 86400) ** 2
         self.data['a_cu'] = (self.data['distance_km'] * 1000) ** 3
@@ -78,10 +83,34 @@ class Moons:
         return self.model_y
 
     def testing(self):
-        test_predict_y = self.model_y.predict(self.x_test)
-        return r2_score(self.y_test, test_predict_y)
+        self.test_predict_y = self.model_y.predict(self.x_test)
+        self.mse = mean_squared_error(self.y_test, self.test_predict_y)
+        self.r2 = r2_score(self.y_test, self.test_predict_y)
+        return self.r2, self.mse
 
     def predict(self):
         G = 6.67e-11
         self.estimated_mass_jupiter = (4 * np.pi ** 2 / G) * self.model_y.coef_[0]
         return self.estimated_mass_jupiter
+
+    def evaluation(self):
+        plt.figure(figsize=(12, 6))
+
+        plt.subplot(1, 2, 1)
+
+        plt.scatter(self.y_test, self.test_predict_y)
+        plt.title('Actual vs Predicted')
+        plt.xlabel('Actual Values')
+        plt.ylabel('Predicted Values')
+
+        plt.subplot(1, 2, 2)
+
+        residuals = self.y_test - self.test_predict_y
+        sns.histplot(residuals, kde=True)
+        plt.title('Residuals Frequency')
+        plt.xlabel('Residuals')
+        plt.ylabel('Frequency')
+
+        plt.tight_layout()
+        plt.show()
+        pass
